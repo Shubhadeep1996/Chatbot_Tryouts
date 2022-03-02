ba40209465e2759e6d3e1bd98c8b5fabadd141ec
Shubhadeep1996 <43546012+Shubhadeep1996@users.noreply.github.com>
2022-03-02 15:27:27 +0530

    Chatbot_College_Enquary First Commit

---
+++ seminar2_progress/shrya/keywords.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+bot = "VIKI-Bot: "
+user = "Anonymous: " 
+
+# Sentences we'll respond with if the user greeted us
+GREETING_KEYWORDS = ("hello", "hi", "greetings", "sup", "what's up","hey","hii")
+
+GREETING_RESPONSES = ["'sup bro", "Hey", "Hello", "Hi"]
+
+add_on_greet = ", my name is VIKI-Bot. You can consult me for RKNEC-related queries!"
+
+user_name = "Anonymous"
+
+def upd_user(name):
+    global user
+    global user_name
+    user_name = name
+    user = user_name + ": "
+
+
+# Sentences we'll respond with if the user greeted us farewell
+FAREWELL_KEYWORDS = ("bye", "sayonara", "astalavista", "farewell","adieu")
+
+FAREWELL_RESPONSES = ["Bye", "Farewell", "See you later", "Sayonara", "Adieu"]
+
+add_on_farewell = "! Hope I was of some help to you"
+
+
+# asking user_name
+ASK_USERNAME = ("May I know your name?", "What is your good name?")
